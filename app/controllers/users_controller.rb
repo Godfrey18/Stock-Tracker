6c645434bf5de99a718d381db8d3e75363dbc0f3
Godfrey18 <godfreymenezes07@gmail.com>
2020-03-10 18:16:57 +0530

    deploy 1stage

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,6 @@
 class UsersController < ApplicationController
   def my_portfolio
+    @user = current_user
   	@tracked_stocks = current_user.stocks
   end
 def my_friends
@@ -28,5 +29,10 @@ def my_friends
       end
     end
 end
+
+def show
+    @user = User.find(params[:id])
+    @tracked_stocks = @user.stocks
+  end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -8,5 +8,6 @@ Rails.application.routes.draw do
  get 'search_stock',to:'stocks#search_stock'
  get 'my_friends',to:'users#my_friends'
  get 'search_friend', to: 'users#search'
+ resources :users, only: [:show]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
