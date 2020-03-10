2b6ccb81929b1ebb4daeebab99750e3e2d4694ea
Godfrey18 <godfreymenezes07@gmail.com>
2020-03-10 17:15:20 +0530

    friendship add delete

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,12 @@
 Rails.application.routes.draw do
  
   resources :user_stocks, only: [:create,:destroy]
+  resources :friendships, only: [:create,:destroy]
   devise_for :users
  root 'welcomes#index'
  get 'my_portfolio',to:'users#my_portfolio'
  get 'search_stock',to:'stocks#search_stock'
+ get 'my_friends',to:'users#my_friends'
+ get 'search_friend', to: 'users#search'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
