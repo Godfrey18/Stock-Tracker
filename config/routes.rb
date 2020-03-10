90796d6b02eab62cdebf615df06a76be85aacd55
Godfrey18 <godfreymenezes07@gmail.com>
2020-03-10 18:22:02 +0530

    switched branch

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,7 @@ Rails.application.routes.draw do
  root 'welcomes#index'
  get 'my_portfolio',to:'users#my_portfolio'
  get 'search_stock',to:'stocks#search_stock'
+ 
  get 'my_friends',to:'users#my_friends'
  get 'search_friend', to: 'users#search'
  resources :users, only: [:show]
